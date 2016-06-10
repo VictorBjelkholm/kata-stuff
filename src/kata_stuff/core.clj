6c7fbdc0970ba84c8340884d963ab7aaa9d76a5e
Victor Bjelkholm <victorbjelkholm@gmail.com>
2016-06-10 16:02:12 +0200

    Work

--- src/kata_stuff/core.clj
+++ src/kata_stuff/core.clj
@@ -1,7 +1,25 @@
 (ns kata-stuff.core
   (:gen-class))
 
+(def list-of-things (atom '()))
+
+(def price-map
+  {"apple" 100
+   "cherry" 75})
+
+(def rebate-map
+  {o
+    })
+
+(defn get-price-by-name
+  [name]
+  (get price-map name))
+
+(defn sum-price-for-products
+  [things]
+  (reduce + (map #(get-price-by-name %1) things)))
+
 (defn -main
-  "I don't do a whole lot ... yet."
   [& args]
-  (println (first args)))
+  (swap! list-of-things concat args)
+  (println (sum-price-for-products @list-of-things)))
